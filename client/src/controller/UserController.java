47343fb04fa68b89f4fe1844667f1f89860204e7
Mahdi <mahdisemsar404@gmail.com>
2024-06-04 11:49:35 +0330

    new class: UserController added to controller package

---
+++ client/src/controller/UserController.java
@@ -0,0 +1,51 @@
+package controller;
+
+
+import model.UserAccount;
+
+import java.io.*;
+import java.net.Socket;
+
+public class UserController {
+    private static UserController userController;
+    private final Socket client;
+    private final PrintWriter out;
+    private final BufferedReader in;
+
+    private UserController() throws IOException {
+        client = new Socket("localhost", 8085);
+        out = new PrintWriter(new BufferedOutputStream(client.getOutputStream()), true);
+        in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+    }
+
+    public static UserController getUserController() throws IOException {
+        if (userController == null) {
+            userController = new UserController();
+        }
+        return userController;
+    }
+
+    private UserAccount userAccount;
+
+    public void setUserAccount(UserAccount userAccount) {
+        this.userAccount = userAccount;
+    }
+
+    public UserAccount getUserAccount() {
+        return userAccount;
+    }
+
+    public Socket getClient() {
+        return client;
+    }
+
+    public PrintWriter getOut() {
+        return out;
+    }
+
+    public BufferedReader getIn() {
+        return in;
+    }
+
+    public UserAccount signUp() {}
+}
