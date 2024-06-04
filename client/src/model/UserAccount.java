29ad159feaf08e67034f8031738b4e752a74b9eb
Mahdi <mahdisemsar404@gmail.com>
2024-06-04 10:22:17 +0330

    two classes: UserAccount and Message added to model and the basics implemented

---
+++ client/src/model/UserAccount.java
@@ -0,0 +1,49 @@
+package model;
+
+public class UserAccount {
+    private String name;
+    private String username;
+    private String password;
+    private String phoneNumber;
+    // maybe need a list of contacts
+    // also maybe need a new table in database to storage them.
+
+    public UserAccount(String name, String username, String password, String phoneNumber) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
